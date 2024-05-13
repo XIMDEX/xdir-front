0570d2b8d6f3d17ddb4e5d6c87b71925cb58b668
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-13 10:50:51 +0200

    Update xdir.service.js

--- src/service/xdir.service.js
+++ src/service/xdir.service.js
@@ -23,7 +23,7 @@ export const loginXDIR = async (email, password) => {
         const json = await res.json()
         return json;
     } catch (err) {
-        return { error: "Check your credentials and try again later." };
+        return { error: err };
     }
 };
 
@@ -35,9 +35,13 @@ export const registerXDIR = async (user) => {
             headers: commonHeaders,
             body: JSON.stringify(user),
         });
-        if(!res.ok) throw new Error (res.error.messagge ?? 'Check your information and try again later.')
-        const json = await res.json()
-        return json;
+        
+        if (!res.ok) {
+            const errorData = await res.json();
+            throw new Error(errorData.message || 'Check your information and try again later.');
+        }
+
+        return await res.json();
     } catch (err) {
         console.error(err);
         return err;
