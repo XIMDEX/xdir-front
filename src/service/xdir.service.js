ade65f167f69085a8cfe32be9087b5753115953d
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-13 11:51:53 +0200

    Update xdir.service.js

--- src/service/xdir.service.js
+++ src/service/xdir.service.js
@@ -6,7 +6,6 @@ const getToken = () =>{
 const commonHeaders = {
     Accept: "application/json",
     "Content-Type": "application/json",
-    Authorization: getToken()
 };
 
 export const loginXDIR = async (email, password) => {
@@ -23,7 +22,7 @@ export const loginXDIR = async (email, password) => {
         const json = await res.json()
         return json;
     } catch (err) {
-        return { error: err };
+        return { error: "Check your credentials and try again later." };
     }
 };
 
@@ -43,8 +42,7 @@ export const registerXDIR = async (user) => {
 
         return await res.json();
     } catch (err) {
-        console.error(err);
-        return err;
+        return { error: err };
     }
 };
 
@@ -53,7 +51,10 @@ export const updateUserXDIR = async (user) => {
     try {
         const res = await fetch(`${API_BASE_URL}api/user/update`, {
             method: "PUT",
-            headers: commonHeaders,
+            headers: {
+                commonHeaders,
+                Authorization: getToken()
+            },
             body: JSON.stringify(user),
         });
         if (!res.ok) {
@@ -70,7 +71,10 @@ export const getRoles = async () => {
     try {
         const res = await fetch(`${API_BASE_URL}api/roles`, {
             method: "GET",
-            headers: commonHeaders,
+            headers: {
+                commonHeaders,
+                Authorization: getToken()
+            },
         });
         if (!res.ok) {
             throw new Error("Failed to fetch roles. Please try again later.");
@@ -87,7 +91,10 @@ export const  createNewRole = async (name) => {
     try {
         const res = await fetch(`${API_BASE_URL}api/role/create`, {
             method: "POST",
-            headers: commonHeaders,
+            headers: {
+                commonHeaders,
+                Authorization: getToken()
+            },
             body: JSON.stringify({
                 name: name
             }),
@@ -107,7 +114,10 @@ export const updateExistingRole = async (id, name) => {
     try {
         const res = await fetch(`${API_BASE_URL}api/role/update/${id}`, {
             method: "PUT",
-            headers: commonHeaders,
+            headers: {
+                commonHeaders,
+                Authorization: getToken()
+            },
             body: JSON.stringify({
                 name: name
             }),
@@ -127,7 +137,10 @@ export const deleteExistingRole = async (id) => {
     try {
         const res = await fetch(`${API_BASE_URL}api/role/delete/${id}`, {
             method: "DELETE",
-            headers: commonHeaders,
+            headers: {
+                commonHeaders,
+                Authorization: getToken()
+            },
         });
         if (!res.ok) {
             throw new Error("Failed to delete this role. Please try again later.");
