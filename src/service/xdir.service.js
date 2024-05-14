d241444f5c8fa6f8592e5ec6d7a783e3816c61f3
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-14 15:21:13 +0200

    organization endpoints

--- src/service/xdir.service.js
+++ src/service/xdir.service.js
@@ -175,16 +175,68 @@ export const assignPermissionToRole = async (id, permission) => {
 }
 
 export const createNewOrganization = async (name) => {
-
+    try {
+        const res = await fetch(`${API_BASE_URL}api/organization/create`, {
+            method: "POST",
+            headers: {
+                commonHeaders,
+                Authorization: `Bearer ${getToken()}`
+            },
+            body: JSON.stringify({
+                name: name
+            }),
+        });
+        if (!res.ok) {
+            throw new Error("Failed to create new organization. Please try again later.");
+        }
+        const json = await res.json();
+        return json;
+    } catch (err) {
+        return { error: "Unable to create new organization. Please try again later." };
+    }
 }
 
 export const updateExistingOrganization = async (id, name) => {
-
+    try {
+        const res = await fetch(`${API_BASE_URL}api/organization/update/${id}`, {
+            method: "PUT",
+            headers: {
+                commonHeaders,
+                Authorization: `Bearer ${getToken()}`
+            },
+            body: JSON.stringify({
+                name: name
+            }),
+        });
+        if (!res.ok) {
+            throw new Error("Failed to update this organization. Please try again later.");
+        }
+        const json = await res.json();
+        return json;
+    } catch (err) {
+        return { error: "Unable to update this organization. Please try again later." };
+    }
 }
 
 export const deleteExistingOrganization = async (id) => {
-    
+    try {
+        const res = await fetch(`${API_BASE_URL}api/organization/delete/${id}`, {
+            method: "DELETE",
+            headers: {
+                commonHeaders,
+                Authorization: `Bearer ${getToken()}`
+            },
+        });
+        if (!res.ok) {
+            throw new Error("Failed to delete this organization. Please try again later.");
+        }
+        const json = await res.json();
+        return json;
+    } catch (err) {
+        return { error: "Unable to delete this organization. Please try again later." };
+    }
 }
+
 export const createNewClient = async (name) => {
 
 }
