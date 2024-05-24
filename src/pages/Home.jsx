d0672f2bc73c9a5e051e5c35af2a4c362fd78a77
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-24 16:35:11 +0200

    add tools to ghome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,85 +7,107 @@ import { XLabel } from '@ximdex/xui-react/material';
 import { StyledHomeItem } from '../components/styled-compontent/Links';
 import { StyledHomeXBox } from '../components/styled-compontent/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import xevalTool from '../assets/logotipo_ximdex-EVAL-small.png'
+import xdamTool from '../assets/logotipo_ximdex-DAM-small-header.png'
 
 function Home() { 
     const [homeButtons, setHomeButtons] = useState([])
+    const [toolsButtons, setToolsButtons] = useState([
+        {
+        name: 'XEVAL',
+        toolSrc: xevalTool,
+        path: '/'
+    },
+{
+        name: 'XDAM',
+        toolSrc: xdamTool,
+        path: '/'
+    }])
     const { isAdmin, isSuperAdmin } = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
-        let buttons = []
-        if(isAdmin || isSuperAdmin){
-            buttons = [{
-                name: 'users',
-                icon: faUsers,
-                path: '/users'
-            }, {
-                name: 'roles',
-                icon: faKey,
-                path: '/roles'
-            }, 
-            ]
+        const buttons = [];
+        if (isAdmin || isSuperAdmin) {
+            buttons.push(
+                {
+                    name: 'users',
+                    icon: faUsers,
+                    path: '/users'
+                },
+                {
+                    name: 'roles',
+                    icon: faKey,
+                    path: '/roles'
+                }
+            );
         }
-        if(isSuperAdmin) buttons.push({
-            name: 'organizations',
-            icon: faBuilding,
-            path: '/organizations'
-        })
-        setHomeButtons(buttons)
-    }, []);
+    
+        if (isSuperAdmin) {
+            buttons.push({
+                name: 'organizations',
+                icon: faBuilding,
+                path: '/organizations'
+            });
+        }
+    
+        setHomeButtons(buttons);
+    }, [isAdmin, isSuperAdmin]);
 
+    const renderButton = (element, index) => (
+        <StyledHomeItem
+            key={'regularElement' + index}
+            to={element.path}
+            style={{
+                color: '#214F61',
+                textTransform: 'uppercase',
+            }}
+        >
+            <XLabel
+                style={{ width: '100%' }}
+                label={element.name}
+                paddingSize='s'
+                component={
+                    <StyledHomeXBox
+                        width={200}
+                        minHeigthContent={200}
+                        id={element.id}
+                        className="xboxItem"
+                    >
+                        {element.toolSrc && <img src={element.toolSrc} alt={element.name} />}
+                        {element.icon && <FontAwesomeIcon icon={element.icon} size='6x' />}
+                    </StyledHomeXBox>
+                }
+                htmlFor={element.name}
+                labelLocation='bottom'
+            />
+        </StyledHomeItem>
+    );
 
-    return ( 
+    return (
         <>
-        <Stack 
-            spacing={2} 
-            direction="row" 
-            width={'100%'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            height={'100%'}
-        > 
-            {homeButtons.map((element, index) => (
-                <StyledHomeItem
-                    key={'regularElement' + index}
-                    to={element.path}
-                    style={{ 
-                        color: '#214F61',
-                        textTransform: 'uppercase',
-                     }}>
-                    <XLabel
-                        style={{ width: '100%'}}
-                        label={element.name}
-                        paddingSize='s'
-                        component=
-                        {<StyledHomeXBox
-                            width={'200'}
-                            minHeigthContent={'200'}
-                            id={element.id} className="xboxItem">
-                            <FontAwesomeIcon icon={element.icon} size='6x' />
-                        </StyledHomeXBox>}
-                        htmlFor={element.name}
-                        labelLocation='bottom'
-                    />
-                </StyledHomeItem>
-            ))}
-            {/* <StyledP>Index</StyledP>
-        
-                {isAdmin || isSuperAdmin ? (
-                <>
-                    <Button style={{margin: '10px'}} variant="contained" color="primary" onClick={() => {navigate('/users')}}>Manage Users</Button>
-                    {isSuperAdmin ? (
-                        <Button style={{margin: '10px'}} variant="contained" color="primary" onClick={() => {navigate('/clients')}}>Manage Client</Button>
-                    ) : (null)}
-                    <Button style={{margin: '10px'}} variant="contained" color="primary" onClick={() => {navigate('/roles')}}>Manage Roles</Button>
-                    <Button style={{margin: '10px'}} variant="contained" color="primary" onClick={() => {navigate('/organizations')}}>Manage Organizations</Button>
-                </>
-                ) : ( null )}
-                <Button style={{margin: '10px'}} variant="contained" color="primary" onClick={() => {navigate('/user')}}>My Profile</Button>
-            */}
-            </Stack> 
+            <Stack
+                spacing={2}
+                direction="row"
+                width={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                height={'100%'}
+            >
+                {homeButtons.map(renderButton)}
+            </Stack>
+            <Stack
+                spacing={2}
+                direction="row"
+                width={'100%'}
+                justifyContent={'center'}
+                alignItems={'center'}
+                height={'100%'}
+            >
+                {toolsButtons.map(renderButton)}
+            </Stack>
         </>
-    ); 
-} 
-export default Home; 
+    );
+}
+
+export default Home;
