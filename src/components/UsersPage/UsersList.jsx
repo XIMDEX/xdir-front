cfd1f9dd828d641dda612e84570c7db4cfdc0b28
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-28 11:47:35 +0200

    Update UsersList.jsx

--- src/components/UsersPage/UsersList.jsx
+++ src/components/UsersPage/UsersList.jsx
@@ -1,14 +1,16 @@
 import { useSpinner } from "@ximdex/xui-react/hooks";
 import React, { useEffect, useState } from "react";
 import { StyledGreenButtonIcon, StyledRedButtonIcon } from "../../components/styled-compontent/Buttons";
-import { XButton, XPopUp, XRowContent, XRowDetails } from "@ximdex/xui-react/material";
+import { XButton, XPopUp, XRowContent, XRowDetails, XRowExtraDetails } from "@ximdex/xui-react/material";
 import { faEdit, faPlus, faTrash, faUsers } from "@fortawesome/free-solid-svg-icons";
 import { StyledFlexFullCenter, StyledXRow } from "../../components/styled-compontent/Container";
-import { getUsers } from "../../service/xdir.service";
+import { deleteExistingUser, getUsers } from "../../service/xdir.service";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useModals from "../../hooks/useModals";
 
 export default function UsersList() {
     const [usersList, setUsersList] = useState([])
+    const {XDirModal} = useModals()
     const [loading, setLoading] = useState(false)
     const { showSpinner, hideSpinner } = useSpinner()
     const [refreshList, setRefreshList] = useState(false)
@@ -26,7 +28,17 @@ export default function UsersList() {
         setLoading(false)
     }
 
-    const deleteUser = () => {}
+    const deleteUser = async (userId, userEmail) => {
+      XDirModal({
+          text:`Are you sure you want to delete ${userEmail}?`,
+          title:'Delete user',
+          confirmButtonColor:'#e13144',
+          onConfirmFunction: async () => {
+            await deleteExistingUser(userId)
+            setRefreshList(!refreshList)
+          }
+        })
+    }
 
     const modifyUserRoles = () => {
   
@@ -58,24 +70,31 @@ export default function UsersList() {
                       labelButtonCollapsable={`Show details`}
                       controls={[
                         {
-                            component:<StyledRedButtonIcon onClick={() => deleteOrganization(user.uuid, user.name)}>
+                            component:<StyledRedButtonIcon onClick={() => deleteUser(user.uuid, user.email)}>
                                         <FontAwesomeIcon icon={faTrash} size='1x' title='Delete user' />
                                     </StyledRedButtonIcon>
                         },
                       ]}
                   >
                     <XRowContent key={"XRowContent" + index}>
-                      <p><strong>Id:</strong> {user?.uuid} - {user?.name + ' ' + user?.surname}</p>
+                      <p>{user?.name + ' ' + user?.surname}</p>
                     </XRowContent>
                     <XRowDetails key={"XRowDetails" + index}>
                       <p><strong>Email:</strong> {user?.email}</p>
                     </XRowDetails>
                     <XRowDetails key={"XRowDetails" + index}>
-                      <p><strong>Roles:</strong> {user?.roles}</p>
-                    </XRowDetails>
-                    <XRowDetails key={"XRowDetails" + index}>
                       <p><strong>Organizations:</strong> {user?.organizations}</p>
                     </XRowDetails>
+                    <XRowExtraDetails key={"XRowExtraDetails" + index}
+                      extraDetails={[
+                        {
+                            label: 'User ID',
+                            type: 'text',
+                            value: user?.uuid
+                        }
+                      ]}
+                    />
+                     
                   </StyledXRow>
                 ))}
               </>
