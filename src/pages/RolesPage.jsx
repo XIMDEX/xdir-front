d10ee9367ac0df9b5be92d76680097608f7a8375
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-29 18:13:19 +0200

    Roles list

--- src/pages/RolesPage.jsx
+++ src/pages/RolesPage.jsx
@@ -1,24 +1,20 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useState } from "react";
 import { StyledMarginContent, StyledTabsContainer, StyledXCard } from "../components/styled-compontent/Container";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFileArchive, faFilePrescription, faKey, faPaperPlane, faUsers } from "@fortawesome/free-solid-svg-icons";
-import { XButton, XPopUp } from "@ximdex/xui-react/material";
-import { createNewPermission, sendRegisterInvite } from "../service/xdir.service";
+import { faFileArchive, faKey } from "@fortawesome/free-solid-svg-icons";
+import { XButton } from "@ximdex/xui-react/material";
+import { createNewPermission as createNewPermissionService, createNewRole as createNewRoleService } from "../service/xdir.service";
 import useModals from "../hooks/useModals";
-import useFormValidator from "../hooks/useFormValidatior";
-import { ROLE_TABS, USER_TABS } from "../../CONSTATNS";
+import { ROLE_TABS } from "../../CONSTATNS";
 import CustomTabs from "../components/CustomTabs/CustomTabs";
-import UsersList from "../components/UsersPage/UsersList";
-import UsersInvites from "../components/UsersPage/UsersInvites";
 import RolesList from "../components/RolesPage/RolesList";
 import PermissionsList from "../components/RolesPage/PermissionsList";
 
-
 export default function RolesPage() {
-  const {XDirModalInput, executeXPopUp} = useModals()
-  const [tabSelected, setTabSelected] = useState(ROLE_TABS[0])
-  const [refreshRoles, setRefreshRoles] = useState(false)
-  const [refreshPermissions, setRefreshPermissions] = useState(false)
+  const { XDirModalInput, executeXPopUp } = useModals();
+  const [tabSelected, setTabSelected] = useState(ROLE_TABS[0]);
+  const [refreshRoles, setRefreshRoles] = useState(false);
+  const [refreshPermissions, setRefreshPermissions] = useState(false);
 
   const createNewRole = async () => {
     const newName = await XDirModalInput({
@@ -30,13 +26,13 @@ export default function RolesPage() {
           return "Please insert a valid name";
         }
       },
-    })
-    if(newName){
-      const res = await createNewRole(newName)
-      executeXPopUp(res, 'Role created successfully')
-      setRefreshRoles(!refreshRoles)
+    });
+    if (newName) {
+      const res = await createNewRoleService(newName);
+      executeXPopUp(res, 'Role created successfully');
+      setRefreshRoles(!refreshRoles);
     }
-  }
+  };
 
   const createNewPermission = async () => {
     const newName = await XDirModalInput({
@@ -48,44 +44,42 @@ export default function RolesPage() {
           return "Please insert a valid name";
         }
       },
-    })
-    if(newName){
-      const res = await createNewPermission(newName)
-      executeXPopUp(res, 'Permission created successfully')
+    });
+    if (newName) {
+      const res = await createNewPermissionService(newName);
+      executeXPopUp(res, 'Permission created successfully');
+      setRefreshPermissions(!refreshPermissions);
     }
-    setRefreshPermissions(!refreshPermissions)
-  }
+  };
 
   return (
     <StyledXCard
-    title={<p style={{marginLeft: '1em'}}><FontAwesomeIcon icon={faKey} style={{marginRight: '10px'}}/>USERS</p>}
-    style={{height: 'auto', width: '80%', margin: '2em auto'}}
-    controls={[
-      {
-          component:
-              <XButton
-                  onClick={tabSelected === 'Roles' ? createNewRole : createNewPermission}
-                  title={`${tabSelected === 'Roles' ? 'Create new Role' : 'Create new Permission'}`}
-              >
-                  <FontAwesomeIcon icon={tabSelected === 'Roles' ? faKey : faFileArchive} style={{marginRight: '10px'}}/> 
-                  {`${tabSelected === 'Roles' ? 'Create role' : 'Create Permission'}`}
-              </XButton>
-      },
-    ]}
-  >
-    <StyledMarginContent>
-      <CustomTabs
-        tabs={ROLE_TABS}
-        setTabSelected={setTabSelected}
-      />
-      <StyledTabsContainer>
-        {tabSelected === 'Roles' && <RolesList refreshRoles={refreshRoles}/>}
-        {tabSelected === 'Permissions' && <PermissionsList/>}
-      </StyledTabsContainer>
-    </StyledMarginContent>
-  </StyledXCard>
-
-
-
+      title={<p style={{ marginLeft: '1em' }}><FontAwesomeIcon icon={faKey} style={{ marginRight: '10px' }} />USERS</p>}
+      style={{ height: 'auto', width: '80%', margin: '2em auto' }}
+      controls={[
+        {
+          component: (
+            <XButton
+              onClick={tabSelected === 'Roles' ? createNewRole : createNewPermission}
+              title={`${tabSelected === 'Roles' ? 'Create new Role' : 'Create new Permission'}`}
+            >
+              <FontAwesomeIcon icon={tabSelected === 'Roles' ? faKey : faFileArchive} style={{ marginRight: '10px' }} />
+              {`${tabSelected === 'Roles' ? 'Create role' : 'Create Permission'}`}
+            </XButton>
+          )
+        },
+      ]}
+    >
+      <StyledMarginContent>
+        <CustomTabs
+          tabs={ROLE_TABS}
+          setTabSelected={setTabSelected}
+        />
+        <StyledTabsContainer>
+          {tabSelected === 'Roles' && <RolesList refreshRoles={refreshRoles} />}
+          {tabSelected === 'Permissions' && <PermissionsList />}
+        </StyledTabsContainer>
+      </StyledMarginContent>
+    </StyledXCard>
   );
 }
