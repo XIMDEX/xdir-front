1a0ddccde945916d05c79737b49c8bd4f3b1cc00
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-05-31 10:39:27 +0200

    Create XimdexTools.jsx

---
+++ src/pages/XimdexTools.jsx
@@ -0,0 +1,90 @@
+import React, { useContext, useEffect, useState } from "react";
+import { StyledDivFlexBetween, StyledFlexFullCenter, StyledMarginContent, StyledTabsContainer, StyledXCard, StyledXRow } from "../components/styled-compontent/Container";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useModals from "../hooks/useModals";
+import { faTools } from "@fortawesome/free-solid-svg-icons";
+import { getXimdexTools } from "../service/xdir.service";
+import { useSpinner } from "@ximdex/xui-react/hooks";
+import { XRowContent, XRowDetails, XRowExtraDetails } from "@ximdex/xui-react/material";
+import { StyledTagStatus } from "../components/styled-compontent/Text";
+
+
+export default function XimdexTools() {
+  const {showSpinner, hideSpinner} = useSpinner()
+  const [loading, setLoading] = useState(false)
+  const [toolsList, setToolsList] = useState([])
+
+  useEffect(() => {
+    getTools()
+  }, []);
+
+  const getTools = async () => {
+    setLoading(true)
+    showSpinner()
+    const res = await getXimdexTools()
+    console.log(res.tools);
+    setToolsList(res.tools)
+    hideSpinner()
+    setLoading(false)
+  }
+
+  return (
+    <StyledXCard
+      title={<p style={{marginLeft: '1em'}}><FontAwesomeIcon icon={faTools} style={{marginRight: '10px'}}/>TOOLS</p>}
+      style={{height: 'auto', width: '80%', margin: '2em auto'}}
+    >
+    <StyledMarginContent>
+    {loading ?  <></> :
+          <>
+            {toolsList.length === 0 ? 
+              <StyledFlexFullCenter>
+                <p>No tools have been created yet.</p>
+              </StyledFlexFullCenter>
+            :
+              <>
+                {toolsList.map((tool, index) => (
+                  <StyledXRow
+                      style={{
+                          background: 'rgb(247, 247, 247)',
+                          width: '100%'
+                      }}
+                      key={'row' + index}
+                      identifier={tool.uuid}
+                      isCollapsable={true}
+                      labelButtonCollapsable={`Show details`}
+                      controls={[]}
+                  >
+                    <XRowContent key={"XRowContent" + index} style={{width: '100%'}}>
+                        <p style={{marginLeft: '10px'}}>{tool?.name}</p>
+                    </XRowContent>
+                    <XRowDetails key={"XRowDetails" + index}>
+                      <p><strong>Description:</strong> {tool.description}</p>
+                    </XRowDetails>
+                    <XRowDetails key={"XRowDetails" + index}>
+                      <p><strong>Type:</strong> {tool.type}</p>
+                    </XRowDetails>
+                    <XRowDetails key={"XRowDetails" + index}>
+                      <p><strong>Status:</strong> {tool.status}</p>
+                    </XRowDetails>
+                    <XRowExtraDetails key={"XRowExtraDetails" + index}
+                      extraDetails={[
+                        {
+                            label: 'User ID',
+                            type: 'text',
+                            value: tool?.uuid
+                        }
+                      ]}
+                    />
+                     
+                  </StyledXRow>
+                ))}
+              </>
+            }
+          </>  
+      }
+    </StyledMarginContent>
+     
+  </StyledXCard>
+
+  );
+}
