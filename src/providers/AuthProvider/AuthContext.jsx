a86fbba8191d005ad02408b73145e6135da46a6c
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-06-03 12:14:15 +0200

    remove autprovider local

--- src/providers/AuthProvider/AuthContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react";
-
-const AuthContext = createContext();
-
-export default AuthContext;
-
-export const useAuth = () => {
-    return useContext(AuthContext);
-  };
